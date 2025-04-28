233ecf8483e75656a3bbb24a3deaa5621cd5297c
Nikita Landarenko <landarenko_nikita@mail.ru>
2025-04-29 01:08:49 +0500

    Add primary key annotation

--- data/liked_courses/src/main/java/com/example/liked_courses/local/entities/LikedCourseDb.kt
+++ data/liked_courses/src/main/java/com/example/liked_courses/local/entities/LikedCourseDb.kt
@@ -2,6 +2,11 @@ package com.example.liked_courses.local.entities
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity(tableName = "liked_course")
-internal data class LikedCourseDb(@ColumnInfo("course_id") val courseId: Long)
+internal data class LikedCourseDb(
+    @PrimaryKey
+    @ColumnInfo("course_id")
+    val courseId: Long
+)
