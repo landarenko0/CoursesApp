82849fb83b411bd15e42552dcf179c12e57594a4
Nikita Landarenko <landarenko_nikita@mail.ru>
2025-04-29 01:07:45 +0500

    Add serialization plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlinx.serialization)
 }
 
 android {
@@ -49,9 +50,20 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.koin)
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.kotlinx.serialization)
 
     implementation(project(":feature:core"))
     implementation(project(":feature:onboarding"))
+    implementation(project(":feature:login"))
+
+    implementation(project(":domain:auth"))
+    implementation(project(":domain:courses"))
+    implementation(project(":domain:liked_courses"))
+
+    implementation(project(":data:courses"))
+    implementation(project(":data:liked_courses"))
 
     testImplementation(libs.junit)
 
