239b46867fe841863b67ffd9bac4ec06a1486a83
landarenko0 <landarenko_nikita@mail.ru>
2025-05-03 14:43:23 +0500

    Set param Ignore on insert conflict strategy to prevent app crash when user clicks on like icon very quickly

--- data/liked_courses/src/main/java/com/example/liked_courses/local/dao/LikedCoursesDao.kt
+++ data/liked_courses/src/main/java/com/example/liked_courses/local/dao/LikedCoursesDao.kt
@@ -3,6 +3,7 @@ package com.example.liked_courses.local.dao
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.example.liked_courses.local.entities.LikedCourseDb
 import kotlinx.coroutines.flow.Flow
@@ -13,7 +14,7 @@ internal interface LikedCoursesDao {
     @Query("SELECT * FROM liked_course")
     fun getLikedCourses(): Flow<List<LikedCourseDb>>
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insertLikedCourse(likedCourse: LikedCourseDb)
 
     @Delete
